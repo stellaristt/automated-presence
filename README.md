c4c3e2d37244fbad33da00aed795ffa8494d1f81
re4ctx <33173989+re4ctx@users.noreply.github.com>
2024-02-22 17:31:23 +0700

    first commit
    uhuyy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# automated-presence
+# Automated Presence | Presensi Otomatis
+
+Projek ini berangkat dari permasalahan yang penulis alami ketika mengikuti kegiatan secara daring yang mengharuskan melakukan presensi di jam tertentu. Tentu menjadi sebuah masalah ketika penulis sedang mengalami insomnia sampai pagi, dan memerlukan setidaknya 2-3 jam untuk tidur. Program ini dibuat untuk melakukan presensi otomatis.
+
+Projek ini dibuat dengan menggunakan Vanilla JavaScript, NodeJS, Selenium Web Driver, dan Node-Cron
---
+++ absenSore.js
@@ -0,0 +1,57 @@
+const { Builder, By } = require("selenium-webdriver");
+const cron = require('node-cron');
+
+async function absenSore() {
+  const driver = await new Builder().forBrowser("chrome").build();
+
+  try {
+    await driver.get("ISI_URL_DISINI");
+
+    await delay(2000);
+
+    await driver.findElement(By.className('button-presence')).click();
+
+    await delay(2000);
+    
+    // pulang
+    await driver.findElement(By.className('button-leave')).click();
+
+    await delay(2000);
+    
+    await driver.findElement(By.css('input[type="number"]')).sendKeys('ISI_ID/USERNAME_DISINI')
+    
+    await driver.findElement(By.css('input[type="password"]')).sendKeys('ISI_PASSWORD_DISINI');
+
+    await delay(2000);
+    
+    await driver.findElement(By.className('button-send')).click();
+
+    await delay(2000);
+
+    await driver.findElement(By.className('check')).click();
+
+    await delay(2000);
+
+    await driver.findElement(By.css('textarea')).sendKeys('Pembekalan Online');
+
+    await delay(2000);
+
+    await driver.findElement(By.xpath('//button[.//span[text()="Konfirmasi"]]')).click();
+
+  } catch (e) {
+        console.log(e);
+  } finally {
+    console.log("Absen sore berhasil!");
+    // await driver.quit();
+  }
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+cron.schedule('0 17 * * *', () => {
+    absenSore();
+},{
+    timezone: "Asia/Jakarta"
+});
